dc39e923a2aea3d814cf5f41693d2a07f8030c4f
Richard Bao <richardbao419@gmail.com>
2018-11-03 15:33:57 -0700

    update

--- detect/main.py
+++ detect/main.py
@@ -3,6 +3,9 @@ import numpy as np
 import math
 from matplotlib import pyplot as plt
 
+#=================================================================================# 
+# Image Reduction 
+#=================================================================================# 
 def preprocess(filename, output_directory, isPath = True):
 	img = filename
 	if (isPath):
@@ -27,6 +30,9 @@ def gradient_elimination(filename, output_directory, isPath = True):
 	cv2.imwrite(output_directory+"THRESH_OTSU.jpg",thresh)
 	return thresh
 
+#=================================================================================# 
+# Hough Lines 
+#=================================================================================# 
 def outline(filename, output_directory, isPath = True):
 	img = filename
 	if (isPath):
@@ -106,8 +112,9 @@ def drawLines(filename, output, lines):
 			cv2.line(img,(x1,y1),(x2,y2),(0,255,0),6)
 	cv2.imwrite(output,img)
 
-#=================================================================================
-
+#=================================================================================# 
+#Filtering and Merging Hough Lines with Slope and Distance 
+#=================================================================================#
 def perpendicular_distance(x1, y1, lx1, ly1, lx2, ly2):
 	slope, intercept = get_equation(lx1,ly1,lx2,ly2)
 	if (slope == math.inf or intercept == math.inf):
@@ -156,32 +163,32 @@ def purify(lines, filename):
 	img = cv2.imread(filename)
 
 	for line in lines:
-		for x1,y1,x2,y2 in line:
-			slope, intercept = get_equation(x1, y1, x2, y2)
-			if (slope != math.inf):
-				slope = int(round(slope))
+		(x1,y1),(x2,y2) = unpack(line)
+		slope, intercept = get_equation(x1, y1, x2, y2)
+		if (slope != math.inf):
+			slope = int(round(slope))
 
-			slope_exists = contains(purified, slope, 1)
-			if slope_exists:
-				slope = slope_exists
-				existing_line = purified[slope][0][0]
-				for lx1,ly1,lx2,ly2 in existing_line:
-					distance = perpendicular_distance(x2, y2, lx1, ly1, lx2, ly2) 
-					distance = int(10*round(distance/10))
-				if (distance in purified[slope].keys()):
-					purified[slope][distance].append(line)
-				else:
-					purified[slope][distance] = [line]
+		slope_exists = contains(purified, slope, 1)
+		if slope_exists:
+			slope = slope_exists
+			existing_line = purified[slope][0][0]
+			(lx1,ly1),(lx2,ly2)  = unpack(existing_line)
+			distance = perpendicular_distance(x2, y2, lx1, ly1, lx2, ly2) 
+			distance = int(10*round(distance/10))
+			if (distance in purified[slope].keys()):
+				purified[slope][distance].append(line)
 			else:
-				purified[slope] = {}
-				purified[slope][0] = [line]
+				purified[slope][distance] = [line]
+		else:
+			purified[slope] = {}
+			purified[slope][0] = [line]
 
 	# for slope, slope_clusters in purified.items():
 	# 	for distance, distance_clusters in slope_clusters.items():
 	# 		drawLines("test/test.png", "output/purified"+str(slope)+str(distance)+".jpg", distance_clusters)
 	# 		endpoint1, endpoint2 = connect(distance_clusters)
 	# 		cv2.line(img,endpoint1,endpoint2,(0,255,0),6)
-	# 		cv2.imwrite("output/test.jpg", img)
+	# 		cv2.imwrite("output/purified_merged.jpg", img)
 
 	for slope, slope_clusters in purified.items():
 		distances = slope_clusters.keys()
@@ -196,13 +203,7 @@ def purify(lines, filename):
 		right_endpoint1, right_endpoint2 = connect(slope_clusters[biggest])
 		cv2.line(img,left_endpoint1,left_endpoint2,(0,255,0),6)
 		cv2.line(img,right_endpoint1,right_endpoint2,(0,255,0),6)
-		cv2.imwrite("output/test.jpg", img)
-
-#Thinnning algorithm dilates lines then thins to one pixel. This merges all lines
-#Then apply hough transform to get the endpoints (or corner detection to get endooints) 
-#For this, 1 pixel might be too thin so then dilates again so its thick enough
-def thin():
-	return
+		cv2.imwrite("output/purified_merged.jpg", img)
 
 def connect(parallel_lines):
 	endpoint1 = unpack(parallel_lines[0])[0]
@@ -220,7 +221,23 @@ def connect(parallel_lines):
 			endpoint2 = maxpoint
 	return (endpoint1, endpoint2)
 
+#=================================================================================# 
+# Line Segment Detector Algorithm
+#=================================================================================# 
 
+#=================================================================================# 
+# Harris Max Corner Detector
+#=================================================================================# 
+
+#=================================================================================# 
+# Line Segment Detector Algorithm
+#=================================================================================# 
+
+#Thinnning algorithm dilates lines then thins to one pixel. This merges all lines
+#Then apply hough transform to get the endpoints (or corner detection to get endooints) 
+#For this, 1 pixel might be too thin so then dilates again so its thick enough
+def thin():
+	return
 
 # if two lines have the same slope and y interdept, group them
 # if two lines have the same slope but different y intercepts, they are parallel
@@ -231,6 +248,7 @@ def main():
 	INPUT = 'test/test.png'
 	OUTPUT = 'output/'
 	
+	# 
 	gray = preprocess(INPUT, OUTPUT)
 	blur = filter(gray, OUTPUT, False)
 	threshold = gradient_elimination(blur, OUTPUT, False)
--- detect/maxcorner.py
+++ detect/maxcorner.py
@@ -1,32 +1,99 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-filename = 'output/merged.jpg'
-img = cv2.imread(filename)
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray,(5,5),0)
+# Harris Corner 
+def harris_corners(filename, isPath = False, blur = True):
+	img = filename
+	if (isPath):
+		img = cv2.imread(filename)
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	if (blur):
+		blur = cv2.GaussianBlur(gray,(3,3),0)
+		blur = np.float32(blur)
+	else:
+		blur = gray
+	dst = cv2.cornerHarris(blur,2,3,0.04)
+	dst = cv2.dilate(dst,None, iterations = 3)
 
-# find Harris corners
-gray = np.float32(blur)
-dst = cv2.cornerHarris(gray,2,3,0.04)
-dst = cv2.dilate(dst,None)
-ret, dst = cv2.threshold(dst,0.5*dst.max(),255,0)
-dst = np.uint8(dst)
+	img[dst>0.01*dst.max()] = [0,255,0]
 
-#result is dilated for marking the corners, not important
-dst = cv2.dilate(dst,None)
+	plt.imshow(img,cmap = 'gray')
+	plt.title('Corners'), plt.xticks([]), plt.yticks([])
 
-# find centroids
-ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+	plt.show()
 
-# define the criteria to stop and refine the corners
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+# Max Harris Corner
+def max_harris_corners(filename, isPath = False, blur = True):
+	img = filename
+	if (isPath):
+		img = cv2.imread(filename)
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	if (blur):
+		blur = cv2.GaussianBlur(gray,(3,3),0)
+		blur = np.float32(blur)
+	else:
+		blur = gray
+	# find Harris corners
+	dst = cv2.cornerHarris(blur,2,3,0.04)
+	dst = cv2.dilate(dst,None)
+	ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
+	dst = np.uint8(dst)
 
-# Now draw them
-res = np.hstack((centroids,corners))
-res = np.int0(res)
-img[res[:,1],res[:,0]]=[0,0,255]
-img[res[:,3],res[:,2]] = [0,255,0]
+	# find centroids
+	ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
 
-cv2.imwrite('final.png',img)
+	# define the criteria to stop and refine the corners
+	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+	corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+	
+	return corners
+	# # Now draw them
+	# res = np.hstack((centroids,corners))
+	# res = np.int0(res)
+	# img[res[:,1],res[:,0]]=[0,0,255]
+	# img[res[:,3],res[:,2]] = [0,255,0]
+
+	# cv2.imwrite('subpixel5.png',img)
+
+# Shi Tomasi
+
+def shitomasi(filename, isPath = False):
+	img = filename
+	if (isPath):
+		img = cv2.imread(filename)
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	corners = cv2.goodFeaturesToTrack(gray, 10, 0.05, 25)
+	corners = np.float32(corners)
+	return np.squeeze(corners, axis = 1)
+	# for item in corners:
+	#     x, y = item[0]
+	#     cv2.circle(img, (x,y), 5, 255, -1)
+
+	# cv2.imshow("Top corners", img)
+	# cv2.waitKey()
+
+def merge():
+	filename = 'test/test4.png'
+	img = cv2.imread(filename)
+	corners1 = shitomasi(img)
+	corners2 = max_harris_corners(img)
+	for item in corners1:
+	    x, y = item
+	    cv2.circle(img, (x,y), 5, (0,255,0), -1)
+	for item in corners2:
+	    x, y = item
+	    cv2.circle(img, (x,y), 5, (255,0,0), -1)
+
+	cv2.imshow("Top corners", img)
+	cv2.waitKey()
+
+
+# Thinning Algorithm returns image that can be fed into harris or max harris or shi tomasi
+
+
+
+
+merge()
+
+
